89e859770657fa96e62a9275a6d97bfa5c79acfd
YinCQL <71113782+YinCQL@users.noreply.github.com>
2023-05-14 01:21:30 +0800

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,15 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+if $OFOX;
+    then
+        export LC_ALL='C'
+        export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+        export FOX_VERSION=R12.1_1
+        export FOX_VARIANT=A11
+        export OF_MAINTAINER='GitHub'
+fi
